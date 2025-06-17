05d35a0539b9c90a93e7e225ae9a421f33d01e2a
Killathon <huelva21@gmail.com>
2025-06-16 20:01:27 -0400

    Updated DefOfs for first party support

---
+++ Source/v1.6/DefOfs/ABF_FleshTypeDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace ArtificialBeings
+{
+    [DefOf]
+    public static class ABF_FleshTypeDefOf
+    {
+        static ABF_FleshTypeDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ABF_FleshTypeDefOf));
+        }
+
+        [MayRequireMechcloudChemwalkers]
+        public static FleshTypeDef ABF_FleshType_Chemwalker_Base;
+    }
+}
