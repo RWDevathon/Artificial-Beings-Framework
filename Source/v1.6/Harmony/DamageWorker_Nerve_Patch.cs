cfc2d17d16c9dfd2d0afa51684da000037fe9a59
Killathon <huelva21@gmail.com>
2025-06-20 17:27:33 -0400

    Artificial units are immune to nerve damage stuns

---
+++ Source/v1.6/Harmony/DamageWorker_Nerve_Patch.cs
@@ -0,0 +1,44 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace ArtificialBeings
+{
+    public class DamageWorker_Nerve_Patch
+    {
+        // Nerve damage shouldn't affect artificial units, which have no nerves.
+        [HarmonyPatch(typeof(DamageWorker_Nerve), "ApplySpecialEffectsToPart")]
+        public class DamageWorker_Nerve_ApplySpecialEffectsToPart_Patch
+        {
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> insts, ILGenerator generator)
+            {
+                List<CodeInstruction> instructions = new List<CodeInstruction>(insts);
+                MethodInfo targetProperty = AccessTools.PropertyGetter(typeof(RaceProperties), nameof(RaceProperties.IsMechanoid));
+
+                for (int i = 0; i < instructions.Count; i++)
+                {
+                    if (i < instructions.Count - 1 && instructions[i + 1].Calls(targetProperty))
+                    {
+                        yield return new CodeInstruction(OpCodes.Dup); // Load a copy of the Pawn onto the Stack
+                    }
+
+                    yield return instructions[i];
+
+                    if (instructions[i].Calls(targetProperty))
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DamageWorker_Nerve_ApplySpecialEffectsToPart_Patch), nameof(IsArtificial))); // Our function call
+                    }
+                }
+            }
+
+            // Returns true if the pawn is artificial.
+            private static bool IsArtificial(Pawn pawn, bool mechanoid)
+            {
+                return mechanoid || ABF_Utils.IsArtificial(pawn);
+            }
+        }
+    }
+}
