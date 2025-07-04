3e42d07e88c4d40fd1464959d10ce569af7820fd
Killathon <huelva21@gmail.com>
2025-07-04 14:16:34 -0400

    Humanlike drones no longer are casually interactible

---
+++ Source/v1.6/Harmony/PawnUtility_Patch.cs
@@ -0,0 +1,26 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+using System;
+
+namespace ArtificialBeings
+{
+    public class PawnUtility_Patch
+    {
+        // Artificial humanlike drones cannot be casually interacted with. This primarily prevents animals nuzzling them for no good reason.
+        [HarmonyPatch(typeof(PawnUtility), "CanCasuallyInteractNow")]
+        public class PawnUtility_CanCasuallyInteractNow_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Listener(ref Pawn p, bool twoWayInteraction, bool canInteractWhileSleeping, bool canInteractWhileRoaming, bool canInteractWhileDrafted, ref bool __result)
+            {
+                if (ABF_Utils.IsArtificialDrone(p) && p.RaceProps.Humanlike)
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
