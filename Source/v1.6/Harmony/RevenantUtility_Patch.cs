032532d3fd3f86a034eafe29a83be4d1cb077972
Killathon <huelva21@gmail.com>
2025-07-22 16:01:38 -0400

    Drones can't be hypnotised, drones don't have social penalties from others' ideologies

---
+++ Source/v1.6/Harmony/RevenantUtility_Patch.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace ArtificialBeings
+{
+    public class RevenantUtility_Patch
+    {
+        // Non-sapient artificial beings are immune to revenant hypnosis.
+        [HarmonyPatch(typeof(RevenantUtility), nameof(RevenantUtility.ValidTarget))]
+        public class RevenantUtility_ValidTarget_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Listener(ref bool __result, Pawn pawn)
+            {
+                if (ABF_Utils.IsConsideredNonHumanlike(pawn))
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Source/v1.6/Harmony/ThoughtWorker_Precept_IdeoDiversity_Social_Patch.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace ArtificialBeings
+{
+    public class ThoughtWorker_Precept_IdeoDiversity_Social_Patch
+    {
+        // Drones don't care about ideological diversity. Other pawns don't care about the drones' lack of ideology.
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_IdeoDiversity_Social), "ShouldHaveThought")]
+        public class ThoughtWorker_Precept_IdeoDiversity_Social_ShouldHaveThought_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, Pawn otherPawn, ref ThoughtState __result)
+            {
+                if (!ABF_Utils.IsConsideredNonHumanlike(otherPawn))
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
