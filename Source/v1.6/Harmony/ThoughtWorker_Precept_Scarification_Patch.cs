601d4b17ae86b01f18cae57529401f919487e1ce
Killathon <huelva21@gmail.com>
2025-07-28 19:12:18 -0400

    Artificial beings no longer care about not being scarred for ideological purposes

---
+++ Source/v1.6/Harmony/ThoughtWorker_Precept_Scarification_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ArtificialBeings
+{
+    public class ThoughtWorker_Precept_Scarification_Patch
+    {
+        // Artificial beings don't care about being scarred or having a lack of them. Others don't care about them being scarred or having a lack of them.
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_Scarification), "ShouldHaveThought")]
+        public class ThoughtWorker_Precept_Scarification_ShouldHaveThought
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, ref ThoughtState __result)
+            {
+                if (ABF_Utils.IsArtificial(p))
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Source/v1.6/Harmony/ThoughtWorker_Precept_Scarification_Social_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ArtificialBeings
+{
+    public class ThoughtWorker_Precept_Scarification_Social_Patch
+    {
+        // Artificial beings don't care about being scarred or having a lack of them. Others don't care about them being scarred or having a lack of them.
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_Scarification_Social), "ShouldHaveThought")]
+        public class ThoughtWorker_Precept_Scarification_Social_ShouldHaveThought
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, Pawn otherPawn, ref ThoughtState __result)
+            {
+                if (ABF_Utils.IsArtificial(otherPawn))
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
