84ab312fc9e257fa642ccd4f1060aa87ada53822
Killathon <huelva21@gmail.com>
2025-08-10 11:17:24 -0400

    No more porcupine quills in artificial beings

---
+++ Source/v1.6/Harmony/Hediff_PorcupineQuill_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace ArtificialBeings
+{
+    class Hediff_PorcupineQuill_Patch
+    {
+        // Artificial pawns should not suffer porcupine quills. It mostly just painful, so does nothing much to them anyway.
+        [HarmonyPatch(typeof(Hediff_PorcupineQuill), nameof(Hediff_PorcupineQuill.PostAdd))]
+        public static class Hediff_PorcupineQuill_PostAdd_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(DamageInfo? dinfo, Pawn ___pawn, Hediff __instance)
+            {
+                if (ABF_Utils.IsArtificial(___pawn))
+                {
+                    ___pawn.health.RemoveHediff(__instance);
+                }
+            }
+        }
+    }
+}
