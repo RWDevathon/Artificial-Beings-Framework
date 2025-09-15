9e22b2d73915021c90aee61369b3045dbf3dec61
Killathon <huelva21@gmail.com>
2025-09-15 11:29:30 -0400

    Drone traders are now casually interactible so they can trade

--- Source/v1.6/Harmony/PawnUtility_Patch.cs
+++ Source/v1.6/Harmony/PawnUtility_Patch.cs
@@ -6,14 +6,14 @@ namespace ArtificialBeings
 {
     public class PawnUtility_Patch
     {
-        // Artificial humanlike drones cannot be casually interacted with. This primarily prevents animals nuzzling them for no good reason.
+        // Artificial humanlike drones cannot be casually interacted with. This primarily prevents animals nuzzling them for no good reason. Make an exception for traders, which need to be interactible.
         [HarmonyPatch(typeof(PawnUtility), "CanCasuallyInteractNow")]
         public class PawnUtility_CanCasuallyInteractNow_Patch
         {
             [HarmonyPrefix]
             public static bool Listener(ref Pawn p, bool twoWayInteraction, bool canInteractWhileSleeping, bool canInteractWhileRoaming, bool canInteractWhileDrafted, ref bool __result)
             {
-                if (ABF_Utils.IsArtificialDrone(p) && p.RaceProps.Humanlike)
+                if (ABF_Utils.IsArtificialDrone(p) && p.RaceProps.Humanlike && p.trader == null)
                 {
                     __result = false;
                     return false;
